c665e11305199649a3b469f6076c2678b336d222
YoHaiYo <12si47bun@naver.com>
2023-11-30 17:31:10 +0900

    footer 수정

--- html/index.php
+++ html/index.php
@@ -8,8 +8,16 @@
 </head>
 <body>
   <h1><a href="http://ohao.dothome.co.kr/waffle/">메인사이트-바로가기</a></h1>  
-  <?php echo 'G5_THEME_CSS_URL : '.G5_THEME_CSS_URL?><br>
-  <?php echo 'G5_THEME_IMG_URL : '.G5_THEME_IMG_URL?><br>
-  <?php echo 'G5_IS_MOBILE : '.G5_IS_MOBILE?>
+  <?php echo 'G5_URL : '.G5_URL?><br>
+  <?php echo 'G5_PATH : '.G5_PATH?><br>
+
+<?php 
+define('SSH', true);
+if ( !defined('SSH') ) {
+	echo "나가";	
+} else {
+	echo "드루와";
+}; 
+?>
 </body>
 </html>
