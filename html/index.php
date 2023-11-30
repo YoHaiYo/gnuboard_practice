ae7843f09af596141a63b6917dec3317a544ba64
User -2023UFLUK\Administrator <ss@ssmail.com>
2023-11-30 11:13:59 +0900

    세팅후 ignore 정리

---
+++ html/index.php
@@ -0,0 +1,15 @@
+<?php include_once("./_common.php"); ?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>와플대학 만들기</title>
+</head>
+<body>
+  <h1><a href="http://ohao.dothome.co.kr/waffle/">메인사이트-바로가기</a></h1>  
+  <?php echo 'G5_THEME_CSS_URL : '.G5_THEME_CSS_URL?><br>
+  <?php echo 'G5_THEME_IMG_URL : '.G5_THEME_IMG_URL?><br>
+  <?php echo 'G5_IS_MOBILE : '.G5_IS_MOBILE?>
+</body>
+</html>
