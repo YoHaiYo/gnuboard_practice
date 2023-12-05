29485a216e01fa5dd8d746856287eb990a087189
YoHaiYo <12si47bun@naver.com>
2023-12-05 12:15:39 +0900

    조떡 호버시 효과추가

--- html/waffle/theme/ety_boba_fett/ssh/ssh.js
+++ html/waffle/theme/ety_boba_fett/ssh/ssh.js
@@ -1,28 +1,26 @@
-// console.log("ssh")
-/* $(document).ready(function(){
-  $('body').addClass("ssh")
-
-  $(window).on('scroll',function(){
-    
-    if($(window.scrollTop() > 0)) {
-      $('body').addClass('blackmode')
-    } else {
-      $('body').removeClass('blackmode')
-    }
-  })
-}) */
 
 // 스크롤시 화면바꾸기
-$(function() {
-  var scrolltop = $(window).scrollTop(); // 화면 열렸을때 스크롤값  
-  console.log(`화면이 열리자마자 스크롤 상단위치 알려줘 : ${scrolltop}`)
-
-  $(window).scroll(function(){      
+$(function() {  
+  $(window).on('scroll',function(){  
     scrolltop = $(window).scrollTop(); // 스크롤 도중의 스크롤값. 변수명을 같게해서 메모리 아끼기
-    if(scrolltop > 0) {
-      $('body').addClass("blackmode")
+    console.log(`현재스크롤 위치 : ${scrolltop}`)
+    
+    if(scrolltop > 1) {
+      // $(".navbar-brand img").attr("src", "http://www.jawsfood.co.kr/images/common/logo2.png")
+      $('body').addClass("ssh--scroll")
     } else {
-      $('body').removeClass("blackmode")
+      // $(".navbar-brand img").attr("src", "http://www.jawsfood.co.kr/images/common/logo1.png")
+      $('body').removeClass("ssh--scroll")
     }
-  })
+
+  }) 
+
+  $('#ssh--gnb').hover(
+    function() {
+      $('body').addClass("ssh--scroll");
+    },
+    function() {
+      $('body').removeClass("ssh--scroll");
+    })  
+
 })
